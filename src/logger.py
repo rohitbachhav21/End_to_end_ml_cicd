22d99857b110d5ce05d9e2c4266e4fcfe35a6232
Rohit Bachhav <bachhavrohit21@gmail.com>
2026-01-18 11:51:13 +0530

    hyperparameter tuning

--- src/logger.py
+++ src/logger.py
@@ -1,46 +1,4 @@
-# import logging
-# import os
-# from datetime import datetime
 
-# # project root = one level above src
-# PROJECT_ROOT = os.path.abspath(
-#     os.path.join(os.path.dirname(__file__), "..")
-# )
-
-# LOGS_DIR = os.path.join(PROJECT_ROOT, "logs")
-# os.makedirs(LOGS_DIR, exist_ok=True)
-
-# LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-# LOG_FILE_PATH = os.path.join(LOGS_DIR, LOG_FILE)
-
-# logging.basicConfig(
-#     filename=LOG_FILE_PATH,
-#     format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-#     level=logging.INFO,
-#     force=True
-# )
-
-# logger = logging.getLogger()
-
-
-
-# # import logging
-# # import os
-# # from datetime import datetime
-
-# # LOG_DIR = os.path.join(os.getcwd(), "logs")
-# # os.makedirs(LOG_DIR, exist_ok=True)
-
-# # LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-# # LOG_FILE_PATH = os.path.join(LOG_DIR, LOG_FILE)
-
-# # logging.basicConfig(
-# #     filename=LOG_FILE_PATH,
-# #     format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-# #     level=logging.INFO,
-# # )
-
-# # # logger = logging.getLogger()
 
 import logging
 import os
--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,13 @@
 
 import os
 import sys
-
 import numpy as np
 import pandas as pd
 import dill
-
+from sklearn.metrics import r2_score
 from src.exception import CustomException
+from sklearn.model_selection import GridSearchCV
+from src.logger import logging
 
 def save_object(file_path,obj):
     try:
@@ -15,6 +16,70 @@ def save_object(file_path,obj):
         
         with open(file_path,'wb') as file_obj:
             dill.dump(obj,file_obj)
+
+    except Exception as e:
+        raise CustomException(e,sys)
+    
+
+# def evaluate_models(X_train,y_train,X_test,y_test,models,param):
+#     try:
+#         report={}
+
+#         for i in range(len(list(models))):
+#             model=list(models.values())[i]
+#             para = param[list(models.keys())[i]]
+
+#             gs = GridSearchCV(model,para,cv=3)
+#             gs.fit(X_train,y_train)
+
+#             model.set_params(**gs.best_params_)
+#             model.fit(X_train,y_train)
+            
+#             # Train model
+#             # model.fit(X_train,y_train)
+
+#             # Make predictions
+#             y_train_pred=model.predict(X_train)
+#             y_test_pred=model.predict(X_test)
+
+#             # Get r2 score
+#             train_model_score=r2_score(y_train,y_train_pred)
+#             test_model_score=r2_score(y_test,y_test_pred)
+
+#             report[list(models.keys())[i]]=[train_model_score,test_model_score]
+
+#         return report
+
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
+    try:
+        report = {}
+
+        for model_name, model in models.items():
+            logging.info(f"Training model: {model_name}")
+
+            para = param.get(model_name, {})
+
+            if para:
+                gs = GridSearchCV(model, para, cv=3, error_score="raise")
+                gs.fit(X_train, y_train)
+                model.set_params(**gs.best_params_)
+
+            model.fit(X_train, y_train)
+
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            train_score = r2_score(y_train, y_train_pred)
+            test_score = r2_score(y_test, y_test_pred)
+
+            report[model_name] = [train_score, test_score]
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
 
     except Exception as e:
         raise CustomException(e,sys)
