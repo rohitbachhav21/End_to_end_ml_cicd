4cc306885beb8733c78a67d80f4d0392abf751e1
Rohit Bachhav <bachhavrohit21@gmail.com>
2026-01-22 22:03:22 +0530

    Disable application.run for Elastic Beanstalk

--- application.py
+++ application.py
@@ -35,5 +35,5 @@ def predict_datapoint():
     return render_template("home.html", results=results[0])
 
 
-if __name__=="__main__":
-    application.run(host="0.0.0.0")
+# if __name__=="__main__":
+#     application.run(host="0.0.0.0")
